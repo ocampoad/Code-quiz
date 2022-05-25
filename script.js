ac3e9f8d27a1b5f76ba3760dc6fb5fe98abf9724
ocampoad <ocampo.adriane13@gmail.com>
2022-05-24 23:10:37 -0700

    comments, organized script, scores are in order

--- script.js
+++ script.js
@@ -1,44 +1,19 @@
 const containerEl = $(".container");
 const btnContainer = $("#answerButtons")
 
+// hide the other containers at the start of the quiz
 $("#showQuiz").hide();
 $('#showTimer').hide();
 $('#hideHighScores').hide();
 $('#hideHighScoresList').hide();
 
-let timeStart = 30;
-let questionNum = 0;
+let timeStart = 30; // amount of time user has to finish quiz
+let questionNum = 0; // start of index for questions
+let scoreCounter = 0; // number of questions correct
 
-$("#startQuiz").click(function () {
-    $("#showQuiz").show();
-    $('#showTimer').show();
-    $('#hideBanner').hide();
-    const countDown = function () {
-        // console.log(timeStart);
-        timeStart--
-        $("#timerDisplay").text("Time remaining: " + timeStart);
-        $("#currentScore").text("Current score: " + scoreCounter);
-        if (timeStart <= 0) {
-            console.log('stop'); //add another function to end quiz
-            clearInterval(timer);
-            btnContainer.text("");
-            $("#questions").text("");
-            $("#Correct").hide()
-            renderSubmit()
-        } else if (questionNum === 5) {
-            clearInterval(timer);
-            btnContainer.text("");
-            $("#questions").text("");
-            $("#Correct").hide();
-            renderSubmit();
-        }
-    };
+const lastScore = JSON.parse(localStorage.getItem("HighScores"));
 
-    const timer = setInterval(countDown, 1000);
-});
-
-let scoreCounter = 0;
-
+// list/ array of questions, possible answers, and actual answers
 const questionBank = [
     'Which HTML element is used to specify a header for a document or section?',
     'Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?',
@@ -63,27 +38,7 @@ const correctAnswer = [
     "The first div element with class=intro",
 ]
 
-$("#answerButtons").click(function () {
-    if ($(event.target).text() === correctAnswer[questionNum]) {
-        scoreCounter++
-        $("#Correct").removeClass("text-bg-danger");
-        $("#Correct").addClass("text-bg-success");
-        $("#Correct").text("correct!");
-        $("#minusPoints").text("")
-            ;
-    } else {
-        timeStart = timeStart - 3;
-        $("#Correct").removeClass("text-bg-success");
-        $("#Correct").addClass("text-bg-danger");
-        $("#Correct").text("wrong!");
-        $("#minusPoints").text("-3")
-    }
-    questionNum = questionNum + 1;
-    btnContainer.text("");
-    $("#questions").text("");
-    renderQuestions()
-});
-
+// render questions as buttons
 const renderQuestions = () => {
     if (questionNum === 0) {
         $("#questions").append(questionBank[questionNum])
@@ -118,16 +73,15 @@ const renderQuestions = () => {
     }
 }
 
-const lastScore = JSON.parse(localStorage.getItem("HighScores"));
-
+// render username input and score (questions correct + time remaining)
 const renderSubmit = () => {
     btnContainer.text("");
     $('#showTimer').hide();
     $('#hideHighScores').show();
-    if (timeStart <= 0 && questionNum < 4) {
-        $('#hideHighScores').children("h1").text("Time is up")
+    if (timeStart <= 0 && questionNum < 4) { 
+        $('#hideHighScores').children("h1").text("Time is up") // if timer reaches 0 and there are still question remaining --> Banner renders "time is up"
     } else {
-        $('#hideHighScores').children("h1").text("Quiz completed")
+        $('#hideHighScores').children("h1").text("Quiz completed") // if user finishes quiz before timer ends--> Banner renders "quiz completed"
     };
     const finalScore = scoreCounter + timeStart;
     $("#questions").append("<div> Your final score is: " + finalScore + "</div>");
@@ -136,19 +90,24 @@ const renderSubmit = () => {
     $("#nameSubmit").children().click(function () {
         const userName = $("#userName").val().trim();
         if (userName === "") {
-            alert("Please input your name");
+            alert("Please input your name"); // make sures user inputs a name
             return
         };
-        if (lastScore !== null) {
-
-            lastScore["name"][lastScore["name"].length] = userName;
-            lastScore["score"][lastScore["score"].length] = finalScore;
+        if (lastScore !== null) { // checks if there is a local storage already saved
+            for (i = 0; i < lastScore["name"].length; i++) { // finds index where to splice depending on score in descending order
+                if (finalScore > lastScore["score"][i]) {
+                    var spliceHere = i;
+                } else {
+                    var spliceHere = lastScore["name"].length;
+                }
+            }
+            lastScore["name"].splice(spliceHere, 0, userName);
+            lastScore["score"].splice(spliceHere, 0, finalScore);
             var HighScores = {
                 "name": lastScore["name"],
                 "score": lastScore["score"],
             }
-            console.log("null")
-        } else {
+        } else { // if no local storage, use this to initialize score
             var HighScores = {
                 "name": [userName],
                 "score": [finalScore],
@@ -160,19 +119,65 @@ const renderSubmit = () => {
     })
 }
 
-
 const highScoreDisplay = () => {
     $("#showQuiz").hide();
     $("#hideHighScoresList").show();
     $('#hideHighScores').children("h1").text("High Scores")
-    const currentscore = JSON.parse(localStorage.getItem("HighScores"));
-    for (i = 0; i < currentscore["name"].length; i++) {
+    const currentscore = JSON.parse(localStorage.getItem("HighScores")); // get current local storage including recent score
+    for (i = 0; i < currentscore["name"].length; i++) { // renders score based on how the scores are score (name and scores should already be in order)
         $('#highScoreUser1').append("<li class='list-group-item d-flex flex-row mb-3'><div class='mx-4'><div class='fw-bold'>" + currentscore["name"][i] + "</div></div><span class='badge bg-primary' id='highScore1'>" + currentscore["score"][i] + "</span></li>");
     }
-    // $('#highScoreUser1').text(currentscore["name"][]);
-    // $('#highScore1').text(currentscore[0]["score"]);
 }
+// event clicker to start quiz--> show the questions/timer
+// this will also hide the initial banner
+$("#startQuiz").click(function () {
+    $("#showQuiz").show();
+    $('#showTimer').show();
+    $('#hideBanner').hide();
+    const countDown = function () {
+        // console.log(timeStart);
+        timeStart--
+        $("#timerDisplay").text("Time remaining: " + timeStart);
+        $("#currentScore").text("Current score: " + scoreCounter);
+        if (timeStart <= 0) {
+            clearInterval(timer);
+            btnContainer.text("");
+            $("#questions").text("");
+            $("#Correct").hide()
+            renderSubmit()
+        } else if (questionNum === 5) {
+            clearInterval(timer);
+            btnContainer.text("");
+            $("#questions").text("");
+            $("#Correct").hide();
+            renderSubmit();
+        }
+    };
 
+    const timer = setInterval(countDown, 1000);
+});
+// event clicker for choosing choices 
+$("#answerButtons").click(function () {
+    if ($(event.target).text() === correctAnswer[questionNum]) {
+        scoreCounter++
+        $("#Correct").removeClass("text-bg-danger");
+        $("#Correct").addClass("text-bg-success");
+        $("#Correct").text("correct!");
+        $("#minusPoints").text("")
+            ;
+    } else {
+        timeStart = timeStart - 3;
+        $("#Correct").removeClass("text-bg-success");
+        $("#Correct").addClass("text-bg-danger");
+        $("#Correct").text("wrong!");
+        $("#minusPoints").text("-3")
+    }
+    questionNum = questionNum + 1;
+    btnContainer.text("");
+    $("#questions").text("");
+    renderQuestions()
+});
+// reloads page from the start start to play again
 $("#playAgianQuiz").click(function () {
     location.reload()
 });
